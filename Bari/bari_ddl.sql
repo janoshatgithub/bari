76bdd8fe15dc04e184ee6ea79273141a0a9ac9d5
jan.sch.hansen <jan.sch.hansen@gmail.com>
2010-11-06 13:37:08 +0000

    Backup

--- Bari/bari_ddl.sql
+++ Bari/bari_ddl.sql
@@ -8,11 +8,21 @@
     alter table DiscussionMessage 
         drop constraint fk_from_discussiommessage_to_bariuser;
 
+    alter table UserGroup 
+        drop constraint fk_from_usergroup_to_product;
+
+    alter table UserGroup 
+        drop constraint fk_from_usergroup_to_bariuser;
+
     drop table BariCase;
 
     drop table BariUser;
 
     drop table DiscussionMessage;
+
+    drop table Product;
+
+    drop table UserGroup;
 
     create table BariCase (
         id bigint not null generated always as identity,
@@ -32,7 +42,7 @@
     create table BariUser (
         id bigint not null generated always as identity,
         fullname varchar(50) not null,
-        login varchar(20) not null,
+        login varchar(20) not null unique,
         password varchar(20) not null,
         userRole varchar(10) not null,
         version integer not null,
@@ -49,6 +59,22 @@
         primary key (id)
     );
 
+    create table Product (
+        id bigint not null generated always as identity,
+        name varchar(50) not null unique,
+        version integer not null,
+        primary key (id)
+    );
+
+    create table UserGroup (
+        id bigint not null generated always as identity,
+        version integer not null,
+        bariUser_id bigint not null,
+        product_id bigint not null,
+        primary key (id),
+        unique (bariUser_id, product_id)
+    );
+
     alter table BariCase 
         add constraint fk_from_baricase_to_bariuser 
         foreign key (bariUser_id) 
@@ -61,5 +87,15 @@
 
     alter table DiscussionMessage 
         add constraint fk_from_discussiommessage_to_bariuser 
+        foreign key (bariUser_id) 
+        references BariUser;
+
+    alter table UserGroup 
+        add constraint fk_from_usergroup_to_product 
+        foreign key (product_id) 
+        references Product;
+
+    alter table UserGroup 
+        add constraint fk_from_usergroup_to_bariuser 
         foreign key (bariUser_id) 
         references BariUser;
